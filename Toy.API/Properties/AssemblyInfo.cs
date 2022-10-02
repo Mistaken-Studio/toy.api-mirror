b23c7f5a2f9a820def999348e4f9a8feeb77c3d8
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-10-02 13:19:31 +0200

    add .gitlab-ci.yml

--- Toy.API/Properties/AssemblyInfo.cs
+++ Toy.API/Properties/AssemblyInfo.cs
@@ -1,5 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
